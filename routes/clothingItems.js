95a91900867b8e46e32b301306e76e815620ede4
Luis Carrola <lcarrola70@gmail.com>
2025-04-13 12:20:44 -0600

    removed controllers/likes.js, fixed reviewer notes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -11,7 +11,7 @@ const auth = require("../middlewares/auth");
 router.get("/", getClothingItems);
 router.post("/", auth, createClothingItem);
 router.delete("/:itemId", auth, deleteClothingItem);
-router.post("/:itemId/like", auth, addLike);
-router.delete("/:itemId/like", auth, deleteLike);
+router.put("/:itemId/likes", auth, addLike);
+router.delete("/:itemId/likes", auth, deleteLike);
 
 module.exports = router;
