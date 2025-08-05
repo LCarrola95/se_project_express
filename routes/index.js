7328dedf5c609a7ae1b201676433c936b26110c0
Luis Carrola <lcarrola70@gmail.com>
2025-08-04 18:58:40 -0600

    feat: ESlint errors corrected

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,11 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
-const { NOT_FOUND } = require("../utils/custom-errors");
+const { NotFoundError } = require("../utils/custom-errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemsRouter);
 
-router.use((req, res, next) => {
-  return next(new NotFoundError("Requested resource not found"));
-});
+router.use((req, res, next) => next(new NotFoundError("Requested resource not found")));
 
 module.exports = router;
